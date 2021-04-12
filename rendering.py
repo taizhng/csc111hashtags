67d6eee1fca8e1db9c00bfdedf25f16d63310242
Kenneth Miura <kenneth.t.miura@gmail.com>
2021-04-11 21:19:09 -0400

    debugging graph glitch

--- rendering.py
+++ rendering.py
@@ -4,6 +4,7 @@ from typing import List, Tuple
 import networkx as nx
 import matplotlib
 from matplotlib.axes import Axes
+
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
@@ -24,8 +25,9 @@ def generate_dummy_graph(num_nodes: int) -> nx.Graph:
         g.add_node(node_name, partisanship=partisanship)
     for i in range(num_nodes):
         node_name = f'Node {i}'
-        connected_node_name = f'Node {random.randrange(0, num_nodes)}'
-        g.add_edge(node_name, connected_node_name)
+        if random.random() > 0.5:
+            connected_node_name = f'Node {random.randrange(0, num_nodes)}'
+            g.add_edge(node_name, connected_node_name)
 
     return g
 
@@ -45,34 +47,63 @@ def render_neighbours(graph: nx.Graph, graph_plot: Axes, selected_node_name: str
             with_labels=True, ax=graph_plot)
 
 
-def render_graph(graph: nx.Graph, num_nodes: int) -> List[str]:
+def render_graph(graph: nx.Graph, num_nodes: int) -> Tuple[List[str], List[Tuple[str, str]]]:
+    '''
+     Note: The first element of the tuple contains the nodes, and the second element of the tuple
+    is the list of edges
+
+    '''
     # choose a random vertex
     # maybe just use this: https://networkx.org/documentation/stable//reference/classes/generated/networkx.Graph.subgraph.html#networkx.Graph.subgraph
-    start_node = random.choice(list(graph.nodes))
-    return _get_nodes(graph, start_node, num_nodes, set())
+    nodes = []
+    edges = []
+    unvisited_nodes = set(graph.nodes)
+    while num_nodes > 0:
+        start_node = random.choice(tuple(unvisited_nodes))
+        # either continue randomly selecting choices till it hasn't been visited, or remove nodes that are in the list
+        # I'm pretty sure doing removal in the set and then choosing from the set as a tuple is faster? in terms of Big O
+        nodes_clump, edges_clump = _get_nodes_and_edges(graph, start_node, num_nodes, set())
+        num_nodes -= len(nodes_clump)
+        nodes.extend(nodes_clump)
+        edges.extend(edges_clump)
+        for node in nodes_clump:
+            unvisited_nodes.remove(node)
+
+    return nodes, edges
     # NOTE: unfinished, and doesn't work
 
 
-def _get_nodes(graph: nx.Graph, current_node_name: str, num_nodes: int,
-               visited: set[str]) -> \
-        List[str]:
-    """ return list of nodes to include
+def _get_nodes_and_edges(graph: nx.Graph, current_node_name: str, num_nodes: int,
+                         visited: set[str]) -> \
+        Tuple[List[str], List[Tuple[str, str]]]:
+    """ return tuple of list of num_nodes nodes, and a list of the edges between them.
+
+    Decides which nodes to include by choosing random vertices, and including all connected vertices
+    till num_nodes hit
+
+    Note: The first element of the tuple contains the nodes, and the second element of the tuple
+    is the list of edges
+
 
     Preconditions:
         - graph nodes have unique names
 
     """
-    # TODO: make this work
     if num_nodes == 0:
         print(f'number of nodes left{num_nodes}')
-        return []
+        return ([], [])
     else:
         nodes = [current_node_name]
+        edges = []
         visited.add(current_node_name)
         for neighbour in graph.neighbors(current_node_name):
             if neighbour not in visited:
-                nodes.extend(_get_nodes(graph, neighbour, num_nodes - 1, visited))
-        return nodes
+                edges.append((current_node_name, neighbour))
+                neighbour_nodes, neighbours_edges = _get_nodes_and_edges(graph, neighbour,
+                                                                         num_nodes - 1, visited)
+                nodes.extend(neighbour_nodes)
+                edges.extend(neighbours_edges)
+        return (nodes, edges)
 
     # This should be really similar to a graph traversal algorithm?
     # Should make list "nodelist", which we pass to the draw method
@@ -89,10 +120,11 @@ def render_tkinter_gui(graph: nx.Graph, graph_figure: plt.Figure, nodes_list: Li
     frame_graph.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True)
     canvas = FigureCanvasTkAgg(graph_figure, master=frame_graph)
     canvas.draw()
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-    toolbar = NavigationToolbar2Tk(canvas, frame_graph)
+    toolbar = NavigationToolbar2Tk(canvas, frame_graph, pack_toolbar=False)
     toolbar.update()
+
+    toolbar.pack(side=tk.BOTTOM, fill=tk.X)
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
     frame_user_interaction = tk.Frame(master=window, width=200, height=100)
@@ -128,26 +160,38 @@ def render_tkinter_gui(graph: nx.Graph, graph_figure: plt.Figure, nodes_list: Li
 
 if __name__ == '__main__':
     # https://stackoverflow.com/questions/20133479/how-to-draw-directed-graphs-using-networkx-in-python
-    g = generate_dummy_graph(10)
-    node_colours = [g.nodes[node_name]['partisanship'] for node_name in g.nodes()]
-    for node_name in g.nodes():
-        node_partisanship = g.nodes[node_name]['partisanship']
-        print(f'{node_name}: partisanship: {node_partisanship}')
+    g = generate_dummy_graph(50)
+    num_nodes = 7
+    n_nodes, n_edges = render_graph(g, num_nodes)
+    print(f'correct number of nodes: {len(n_nodes) == num_nodes}')
+    print(n_nodes)
+    node_colours = [g.nodes[node_name]['partisanship'] for node_name in n_nodes]
 
     # Color map stuff: https://stackoverflow.com/questions/25748183/python-making-color-bar-that-runs-from-red-to-blue
     blue_to_red = colors.LinearSegmentedColormap.from_list("BlueToRed", ["b", "r"])
     graph_figure = plt.Figure()
     a = graph_figure.add_subplot(111)
-    # render_neighbours(g, "Node 4")
-    nx.draw(g, cmap=blue_to_red, node_color=node_colours, with_labels=True, ax=a)
+    spring_pos = nx.spring_layout(g)
+    # Try just drawing nodes -> draw edges -> draw layers
+    labels = {}
+    for node in n_nodes:
+        labels[node] = node
+    nx.draw(g, pos=spring_pos, cmap=blue_to_red, node_color=node_colours, with_labels=True, ax=a,
+            nodelist=n_nodes, labels=labels, edgelist=n_edges)
+
     render_tkinter_gui(g, graph_figure, list(g.nodes))
 
     # TODO:
-    # figure out how to intelligently limit number of vertices (This can wait, I know this is within capabilities)
-    # figure out how to take input, send that input to network x
+    # figure out how to intelligently limit number of vertices (Basically done)
+    #
     # https://stackoverflow.com/questions/55553845/display-networkx-graph-inside-the-tkinter-window
-    # try making it so blue is on left, red is on right
+    # make it so it doesn't recalculate color when u open a new graph lol
+
+    # NOTE: Jeremy, this is your stuff
     # Why does the application glitch when the mouse is in the boundaries?
+    #   - glitching only happens in pan mode, when the graph is in tkinter.
+    #   - is this just networkX? Or anything through matplotlib
+    #   - Does this happen when you render the whole graph?
 
     # Tkinter: https://realpython.com/python-gui-tkinter/
     # TODO: maybe refactor so all the tkinter stuff is in a class? May be easier to interact with then
