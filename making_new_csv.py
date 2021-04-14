5e7f52a76cd521fed54aaf94bb72aef58049dbae
tzhang_ <41173438+taizhng@users.noreply.github.com>
2021-04-13 22:45:03 -0700

    Correct making_csv, added total filtered.
    Modified Jeremy's making new csv.
    
    Also sent over the total_filtered_politician.csv file.

--- making_new_csv.py
+++ making_new_csv.py
@@ -5,40 +5,48 @@ import json
 import csv
 
 
-def convert_python_to_csv(tweets_file: str, member_info_file: str) -> None:
-    """
-    This function will convert the results from get_us_hashtags to a csv file
-    """
-    filtered_results = get_us_hashtags(tweets_file, member_info_file)
-    with open('filtered_politician.csv', mode='w') as csv_file:
-        fieldnames = ['name', 'partisan_score', 'hashtags']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames, lineterminator='\n')
-        writer.writeheader()
-        for filtered_result in filtered_results:
-            writer.writerow(
-                {'name': filtered_result['name'],
-                 'partisan_score': filtered_result['partisan_score'],
-                 'hashtags': filtered_result['hashtags']})
+# def convert_python_to_csv(tweets_file: str, member_info_file: str) -> None:
+#     """
+#     This function will convert the results from get_us_hashtags to a csv file
+#     """
+#     filtered_results = get_us_hashtags(tweets_file, member_info_file)
+#     with open('partial_filtered_politician.csv', mode='w') as csv_file:
+#         fieldnames = ['name', 'partisan_score', 'hashtags']
+#         writer = csv.DictWriter(csv_file, fieldnames=fieldnames, lineterminator='\n')
+#         writer.writeheader()
+#         for filtered_result in filtered_results:
+#             writer.writerow(
+#                 {'name': filtered_result['name'],
+#                  'partisan_score': filtered_result['partisan_score'],
+#                  'hashtags': filtered_result['hashtags']})
 
 
-def get_us_hashtags(tweets_file: str, member_info_file: str) -> list[dict]:
+def get_us_hashtags(tweets_file: str, member_info_file: str) -> int:
     """
     This function will get rid of all the non us hashtags and return a dictionary with these
     people's name, partisan score(Democrats: 0, Republicans: 1), and hashtags.
     """
-    us_hashtags = []
-    tweets = read_tweets_file_data(tweets_file)
+    unread = 0
     us_politicians = get_us_information(member_info_file)
-    for tweet in tweets:
-        for us_politician in us_politicians:
-            user = tweet['user']
-            # this makes sure that there won't be any empty hashtags
-            if us_politician['name'] == user['name'] and tweet['entities']['hashtags'] != []:
-                politician_info = {'name': user['name'],
-                                   'partisan_score': us_politician['partisan_score'],
-                                   'hashtags': {x['text'] for x in tweet['entities']['hashtags']}}
-                us_hashtags.append(politician_info)
-    return us_hashtags
+    with open('total_filtered_politician.csv', mode='w', encoding='utf-8') as csv_file:
+        fieldnames = ['name', 'partisan_score', 'hashtags']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames, lineterminator='\n')
+        writer.writeheader()
+        with open(tweets_file, encoding="UTF-8") as json_file:
+            print("opened")
+            for line in json_file:
+                tweet = json.loads(line)
+                user = tweet['user']
+                # this makes sure that there won't be any empty hashtags
+                if tweet['entities']['hashtags'] != []:
+                    if user['name'] in us_politicians:
+                        writer.writerow(
+                            {'name': user['name'],
+                             'partisan_score': us_politicians[user['name']],
+                             'hashtags': {x['text'] for x in tweet['entities']['hashtags']}})
+                    else:
+                        unread += 1
+            return unread
 
 
 def read_tweets_file_data(file: str) -> list[dict]:
@@ -53,25 +61,23 @@ def read_tweets_file_data(file: str) -> list[dict]:
     return data
 
 
-def get_us_information(file: str) -> list[dict]:
+def get_us_information(file: str) -> dict[str, int]:
     """
     This function will get all us politician's information
     """
     with open(file, encoding="UTF-8") as file_read:
         reader = csv.reader(file_read)
         next(reader)
-        data = []
+        data = {}
         for row in reader:
             if row[9] == 'United States':
-                politician_info = {'name': row[0]}
                 if row[4] == 'Democrat':
-                    politician_info['partisan_score'] = 0
+                    data[row[0]] = 0
                 elif row[4] == 'Republican':
-                    politician_info['partisan_score'] = 1
-                data.append(politician_info)
+                    data[row[0]] = 1
     return data
 
 
 if __name__ == '__main__':
     # this out put a csv file from the two given file
-    convert_python_to_csv('small_tweet_ids.jsonl', 'full_member_info.csv')
+    get_us_hashtags('all_tweet_ids.jsonl', 'full_member_info.csv')
